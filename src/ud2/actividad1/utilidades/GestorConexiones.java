29ddce7f7e5cdeec86088bd24cad5c60e4a00c3b
Israel Gago <igagoacun@ieschandomonte.edu.com>
2025-12-09 09:25:17 +0100

    Inicio conexiones con bases de datos

--- src/ud2/actividad1/utilidades/GestorConexiones.java
+++ src/ud2/actividad1/utilidades/GestorConexiones.java
@@ -11,7 +11,7 @@ import static ud2.actividad1.utilidades.TipoSGBD.*;
 
 public class GestorConexiones {
     public static Connection getConnection(ud2.actividad1.utilidades.TipoSGBD tipo, String baseDatos, String usuario, String password) throws SQLException {
-        String url = "";
+        String url;
         switch (tipo) {
             case SQLSERVER -> {
                 url = "jdbc:sqlserver://localhost:1433;" +
@@ -23,7 +23,7 @@ public class GestorConexiones {
                 url = "jdbc:mysql://localhost:3306/" + baseDatos + "?serverTimezone=UTC";
             }
             case SQLITE -> {
-                url = "jdbc:sqlite" + baseDatos;
+                url = "jdbc:sqlite://" + baseDatos;
             }
             default -> {
                 throw new UnsupportedOperationException("Tipo SGBD no soportado: " + tipo);
@@ -41,13 +41,23 @@ public class GestorConexiones {
         var meta = con.getMetaData();
         StringBuilder sb = new StringBuilder();
 
-        sb.append("Driver: ").append(meta.getDriverName());
-        sb.append("Versión del driver: ").append(meta.getDriverVersion());
-        sb.append("Producto BD: ").append(meta.getDatabaseProductName());
-        sb.append("Versión BD: ").append(meta.getDatabaseProductVersion());
-        sb.append("Url: ").append(meta.getURL());
-        sb.append("Usuario BD: ").append(meta.getUserName());
+        sb.append("Driver: ").append(meta.getDriverName()).append("\n");
+        sb.append("Versión del driver: ").append(meta.getDriverVersion()).append("\n");
+        sb.append("Producto BD: ").append(meta.getDatabaseProductName()).append("\n");
+        sb.append("Versión BD: ").append(meta.getDatabaseProductVersion()).append("\n");
+        sb.append("Url: ").append(meta.getURL()).append("\n");
+        sb.append("Usuario BD: ").append(meta.getUserName()).append("\n");
 
         return sb.toString();
+    }
+
+    public static void cerrarConexion(Connection con) {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println("Error al cerrar la conexión: " + e.getMessage());
+            }
+        }
     }
 }
