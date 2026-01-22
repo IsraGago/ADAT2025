d7f239b766d6f8cbc3151d1c80acf3021a966e59
Israel Gago <igagoacun@ieschandomonte.edu.com>
2026-01-22 12:27:48 +0100

    EXAMEN BASES DE DATOS

--- src/ud2/examen/Main.java
+++ src/ud2/examen/Main.java
@@ -1,3 +1,4 @@
+package ud2.examen;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
